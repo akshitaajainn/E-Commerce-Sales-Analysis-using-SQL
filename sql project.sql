73c282a44aadf457d5315115690ac14c1c9bec27
akshitaajainn <akshitaaajainn@gmail.com>
2025-06-21 13:53:36 +0530

    Update sql project.sql

--- sql project.sql
+++ sql project.sql
@@ -192,7 +192,7 @@ limit 10;
 -- 2. What percentage of customers are repeat buyers?
 
 select round(sum(case when order_count > 1 then 1 else 0 end)
-  / count(*) * 100, 2) as repeat_buyer_pct
+  / count(*) * 100, 2) as repeat_buyer_%
 from (select customer_id,count(distinct order_id) as order_count
 from orders
 group by customer_id) as customer_orders;
